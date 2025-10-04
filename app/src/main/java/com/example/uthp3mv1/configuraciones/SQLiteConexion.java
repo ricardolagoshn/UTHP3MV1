fa09bae1599eb0947324f85f420e1f4ad79e8fbe
Dell <ricardo.lagoshn@gmail.com>
2025-10-04 16:35:23 -0600

    Cambios al 04/10/2025

---
+++ app/src/main/java/com/example/uthp3mv1/configuraciones/SQLiteConexion.java
@@ -0,0 +1,28 @@
+package com.example.uthp3mv1.configuraciones;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class SQLiteConexion extends SQLiteOpenHelper
+{
+
+    public SQLiteConexion(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(Transacciones.CREATETABLEPERSONAS);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL(Transacciones.DROPTABLEPERSONAS);
+        onCreate(sqLiteDatabase);
+
+    }
+}
